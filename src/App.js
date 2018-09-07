de051f566e69534f5a6bcaee4d595a1239e946b0
thibaut severac <thib3113@gmail.com>
2018-09-07 09:14:40 +0200

    back

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        environment:"dev"
+    };
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to DigitalState docker-compose generator</h1>
+                </header>
+                <p className="App-intro">
+                    Step 1 : Select your services
+                </p>
+                <p>
+                    Step 2 : Select your environment
+                    <select ></select>
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
